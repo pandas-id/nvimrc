0214499419ce23d80fdb0e25b6effc24913d6bec
Akmal <akmalthemalong@gmail.com>
2026-03-14 17:25:26 +0800

    refactor(yazi): ubah pemicu plugin dari event ke cmd, perbaiki dependensi, dan sesuaikan keymap serta deskripsi

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,44 +1,36 @@
 ---@type LazySpec
 return {
 	"mikavilpas/yazi.nvim",
-	event = "VeryLazy",
+	cmd = "Yazi",
 	dependencies = {
-		-- check the installation instructions at
-		-- https://github.com/folke/snacks.nvim
-		"folke/snacks.nvim",
+		{ "folke/snacks.nvim", lazy = true },
 	},
 	keys = {
-		-- 👇 in this section, choose your own keymappings!
 		{
-			"<leader>-",
-			mode = { "n", "v" },
+			"<leader>y",
 			"<cmd>Yazi<cr>",
-			desc = "Open yazi at the current file",
+			mode = { "n", "v" },
+			desc = "Open yazi at current file",
 		},
 		{
-			-- Open in the current working directory
 			"<leader>cw",
 			"<cmd>Yazi cwd<cr>",
-			desc = "Open the file manager in nvim's working directory",
+			desc = "Open yazi in working directory",
 		},
 		{
 			"<c-up>",
 			"<cmd>Yazi toggle<cr>",
-			desc = "Resume the last yazi session",
+			desc = "Resume last yazi session",
 		},
 	},
 	---@type YaziConfig | {}
 	opts = {
-		-- if you want to open yazi instead of netrw, see below for more info
 		open_for_directories = false,
 		keymaps = {
 			show_help = "<f1>",
 		},
 	},
-	-- 👇 if you use `open_for_directories=true`, this is recommended
 	init = function()
-		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-		-- vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 	end,
 }
